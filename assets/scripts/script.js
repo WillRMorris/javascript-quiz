d8b68e721b5cd153a7a0fe47f84a2e13d5b433c4
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-01-14 21:46:00 -0700

    finish landing page functions, local storage is called and sorted into an array before being returned to storage. highscores page nearly functional, and should be visitable through top left hand link throughout any stage of the quiz.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,16 @@
- //TODO additional UI elements to interact with highscore.
- //TODO find proper local storage code and make function for getting final high score and storing it. 
- //TODO js to move additional UI around at quiz beginings. Button also needs to function.
+//TODO add event listeners for go back and clear score. add time subtraction to incorrect answers
  const welcome = document.getElementById("welcome");
  const startBtn = document.getElementById("start-quiz");
  const body = document.body;
+ const viewScores = document.getElementById("a-wrapper");
  var whichQuestion = "none";
+
  
  //Timer object. intiates timer and holds related elements and functions.
  var timer = {
     min: 0,
     sec: 0,
+    time: null,
     clock: document.createElement("div"),
     clockWrapper: document.createElement("div"),
     generate: function (){
@@ -22,7 +23,7 @@
     start: function(){ 
        timer.min = 1;
        timer.sec = 15;
-      var time = setInterval(()=>{
+      timer.time = setInterval(()=>{
           if (timer.sec <= 9) {
           timer.clock.textContent = timer.min + ":0" + timer.sec;
           }
@@ -38,13 +39,15 @@
          };
          if (timer.min === 0 && timer.sec === 0){
             timer.clock.textContent = "0:00";
-            clearInterval(time);
+            clearInterval(timer.time);
+            end.finish();
          };
          return;
       }, 1000);
       return;
    },
    calculateScore: function (){
+      clearInterval(this.time);
       let score = this.min*60 + this.sec;
       return score;
    }
@@ -138,7 +141,6 @@ var quiz = {
          }
          //if there are no more questions the test ends
          else {
-            this.cardWrapper.setAttribute("style" , "display: none;")
             end.finish();
             console.log("end");
          }
@@ -156,11 +158,15 @@ var end = {
    formWrapper: document.createElement("div"),
    intialsInput: document.createElement("input"),
    submit: document.createElement("input"),
-   intitals: "",
+   scoreTableWrapper: document.getElementById("score-wrapper"),
+   scoreTable: document.getElementById("scoreboard"),
+   buttonWrapper: document.createElement("div"),
+   backButton: document.createElement("button"),
+   clearScoresBtn: document.createElement("button"),
    finished: false,
-   
+
    finish: function() {
-      this.finished = true;
+      end.finished = true;
       score = timer.calculateScore();
       body.appendChild(this.cardWrapper);
       this.cardWrapper.appendChild(this.card);
@@ -175,19 +181,106 @@ var end = {
       this.cardWrapper.setAttribute("class", "card");
       this.intialsInput.setAttribute("type" , "text");
       this.submit.setAttribute("type" , "submit");
-
+      
+      this.cardWrapper.setAttribute( "style" , " padding: 50px, 100px; left: 40%; ");
+      this.card.setAttribute("style", "display: flex; flex-direction: column; flex-wrap: nowrap;");
+      this.heading.setAttribute("style" , "text-align: left; margin: 15px;");
+      this.subtitle.setAttribute("style" , "text-align: left; margin: 0 0 15px 15px;");
+      this.formWrapper.setAttribute("style" , "margin: 15px;")
       this.form.setAttribute("style" , "display: flex; align-items: center;justify-contents: center; height:22px;");
       this.intialsInput.setAttribute("class" , "text-input");
       this.submit.setAttribute("id" , "submit");
 
-      this.heading.setAttribute("style" , "text-align: left; margin: 15px;")
-      this.card.setAttribute("style", "display: flex; flex-direction: column; flex-wrap: nowrap;");
-      this.cardWrapper.setAttribute( "style" , " padding: 50px, 100px; width:60%; left: 20%; justify-content: flex-start;");
-
+      quiz.cardWrapper.setAttribute("style" , "display: none;");
       this.heading.textContent = "Finished!";
       this.subtitle.textContent = "your final score is: " + score;
    },
+   scorePage: function () {
+      end.finished = true;
+      body.appendChild(this.scoreTableWrapper);
+      this.scoreTableWrapper.appendChild (this.header);
+      this.header.appendChild(this.heading);
+      this.scoreTableWrapper.appendChild(this.scoreTable);
+      this.scoreTableWrapper.appendChild(this.buttonWrapper);
+      this.buttonWrapper.appendChild(this.backButton);
+      this.buttonWrapper.appendChild(this.clearScoresBtn);
+
+
+
+      
+      this.scoreTableWrapper.setAttribute("class" , "card");
+      this.scoreTableWrapper.setAttribute("style" ,"display: flex; width: 300px;");
+      this.heading.setAttribute("style" , "text-align: left; margin: 15px;");
+      this.heading.textContent = "Highscores";
+      
+      this.scoreTable.setAttribute("id" , "scoreboard");
+      
+      this.buttonWrapper.setAttribute("style" , "display: flex; margin: 15px; justify-contents: flex start;")
+      this.backButton.setAttribute("class" , "btn");
+      this.backButton.setAttribute("style" , "font-size: 15px; text-wrap: nowrap; padding: 15px; margin: 10px 20px;");
+      this.backButton.textContent = "go back";
+      this.clearScoresBtn.setAttribute("class" , "btn");
+      this.clearScoresBtn.setAttribute("style" , "font-size: 15px; text-wrap: nowrap; padding: 15px; margin: 10px 20px;");
+      this.clearScoresBtn.textContent = "clear scores";
+
+
+      this.cardWrapper.setAttribute("style" , "display: none;");
+      this.formWrapper.setAttribute("style" , "display: none;")
+      this.subtitle.setAttribute("style" , "display: none;");
+      quiz.cardWrapper.setAttribute("style" , "display: none;");
+      welcome.setAttribute("style" , "display: none;");
+
+      //--------------------score table-------------------------\\
+      var array = sortStorage();
+      for(var i =0; i< array.length; i++){
+         var sc =array[i];
+         var li = new localScore(i , sc[0] , sc[1]);
+      }
+
+   },
    
+}
+
+class localScore {
+   constructor(n, name, score) {
+      this.n = n;
+      this.name = name;
+      this.score = score;
+      this.line = document.createElement("li");
+      this.line.setAttribute ("class" , "high-score");
+      end.scoreTable.appendChild(this.line);
+      this.line.textContent = `${this.name} -- ${this.score}`
+   };
+
+   
+}
+
+function sortStorage() {
+   var array = [];
+   
+   for(var i = localStorage.length-1; i >=0; i --){
+      let large = "";
+      let tempName = "";
+      var tempscore = [];
+      large =localStorage.getItem(localStorage.key(i))
+      tempName=localStorage.key(i);
+      for(var j = localStorage.length-1; j >= 0; j--){
+         if(large < localStorage.getItem(localStorage.key(j))){
+            large =localStorage.getItem(localStorage.key(j));
+            tempName =localStorage.key(j);
+         }
+      }
+      localStorage.removeItem(tempName);
+      tempscore.push(tempName);
+      tempscore.push(large);
+      array.push(tempscore);
+      console.log(array);
+   }
+   for( let i = 0; i < array.length; i++){
+      var sc = array[i];
+      localStorage.setItem(sc[0], (sc[1]));
+   };
+   return array;
 }
 //function runs if the right answer is chosen.
 function ifCorrect (){
@@ -210,10 +303,14 @@ function ifWrong () {
 };
 //intiates even listeners. was going to do all this in quiz object but was running into errors put it out here for the time being. 
 function buttonsWork () {
+   viewScores.addEventListener("click" , function(){
+      end.scorePage();
+      return;
+   });
    startBtn.addEventListener("click", function() {
       welcome.style.display = "none";
       timer.generate();
-       timer.start();
+      timer.start();
       quiz.generate();
       quiz.nextQuestion();
       return;
@@ -254,7 +351,14 @@ function buttonsWork () {
       else {
          ifWrong();
       }
-      quiz.nextQuestion ();
+      quiz.nextQuestion();
+   });
+   end.submit.addEventListener("click" , function(){
+      event.preventDefault();
+      let intials = end.intialsInput.value;
+      localStorage.setItem(intials, timer.calculateScore());
+      end.scorePage();
+      console.log(localStorage);
    });
 }
 //starts buttons working, which starts the rest of the code running.
